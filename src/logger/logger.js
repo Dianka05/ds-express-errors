2e78fb2248047b11caa78b2eb4eb1d5118cee9d7
Dianka05 <diankastojka5@gmail.com>
2025-12-10 15:51:07 +0100

    fix

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -14,7 +14,7 @@ function logError(error, req) {
         const code = error.code ? `[${error.code}]` : ''
         const name = error.name || ''
 
-        if (checkLoggerExist) {
+        if (checkLoggerExist()) {
             config.customLogger.error(`[${timestamp}] ${req?.method || ''} ${req?.originalUrl || ''} \n[Error]: ${code} ${name} \nMessage: ${message} \nStatusCode: ${statusCode} \n${stack ? `Stack: ${stack}` : ''} \nOperational: ${isOperational}\n`)
         } else {
             console.error(`[${timestamp}] ${req?.method || ''} ${req?.originalUrl || ''} \n[Error]: ${code} ${name} \nMessage: ${message} \nStatusCode: ${statusCode} \n${stack ? `Stack: ${stack}` : ''} \nOperational: ${isOperational}\n`)
@@ -25,7 +25,7 @@ function logError(error, req) {
 function logInfo(message) {
     const timestamp = getTimestamp()
 
-    if (checkLoggerExist) {
+    if (checkLoggerExist()) {
         config.customLogger.info(`[${timestamp}] - [INFO]: ${message}`)
     } else {
         console.log(`[${timestamp}] - [INFO]: ${message}`)
@@ -35,7 +35,7 @@ function logInfo(message) {
 function logWarning(message, req) {
     const timestamp = getTimestamp()
 
-    if (checkLoggerExist) {
+    if (checkLoggerExist()) {
         config.customLogger.warn(`[${timestamp}] - ${req?.method || ''} ${req?.originalUrl || ''} \n[WARNING]: ${message}`)
     } else {
         console.warn(`[${timestamp}] - ${req?.method || ''} ${req?.originalUrl || ''} \n[WARNING]: ${message}`)
@@ -45,7 +45,7 @@ function logWarning(message, req) {
 function logDebug(message, req) {
     const timestamp = getTimestamp()
 
-    if (checkLoggerExist) {
+    if (checkLoggerExist()) {
         config.customLogger.debug(`[${timestamp}] - [DEBUG]: ${req?.method || ''} ${req?.originalUrl || ''} [Message]: ${message}`)
     } else {
         console.debug(`[${timestamp}] - [DEBUG]: ${req?.method || ''} ${req?.originalUrl || ''} [Message]: ${message}`)
